aad18539ad6d8ba1483f313341d21716abcd2cab
Dudeicle <brian-griffiths@lambdastudents.com>
2020-06-10 17:18:47 -0400

    another commit, finally pulling what I think is the right data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Photo from './components/photo'
 import { API_KEY } from './secrets'
+import axios from 'axios'
 
-
-const BASE_URL = ''
-
+const BASE_URL = 'http://localhost:3000/'
 
 export default function App() {
+
+
+  // "https://www.nasa.gov/multimedia/imagegallery/iotd.html?api_key=yeX2aVMl4CEHmu5DxqeL7BNBJQe5cPcGaJYj21cN"
+
+useEffect(() => {
+  axios.get("https://api.nasa.gov/planetary/apod?api_key=yeX2aVMl4CEHmu5DxqeL7BNBJQe5cPcGaJYj21cN")
+    .then(res => {
+      console.log(res)
+      return `${res.data.date}`
+    })
+    .catch(err => {
+      console.log('this is an error message')
+    })
+}, [])
+
 
   return (
     <div className="App">
@@ -21,5 +35,3 @@ export default function App() {
     </div>
   );
 }
-
-
