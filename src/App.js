1a1a31a988d933a68cabc97d744da299d9b1694d
Dudeicle <brian-griffiths@lambdastudents.com>
2020-06-10 16:34:46 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import Photo from './components/photo'
+import { API_KEY } from './secrets'
 
-function App() {
+
+const BASE_URL = ''
+
+
+export default function App() {
+
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
+
+        
       </p>
+      <Photo />
     </div>
   );
 }
 
-export default App;
+
---
+++ src/components/photo.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react'
+
+export default function Photo(props) {
+    return (
+        <div className='photo-container'>
+            <span>hello this is photo container</span>
+        </div>
+    )
+}
